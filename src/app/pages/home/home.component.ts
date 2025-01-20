f5ee5b36f57ab496f337338714566343d7a3ba22
Abdurashid <80646202+abdurashid-juraev@users.noreply.github.com>
2025-01-20 14:06:12 +0500

    filter by pipe

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,39 +4,26 @@ import { NzGridModule } from 'ng-zorro-antd/grid';
 import { CardComponent } from '../../component/components/cards/card/card.component';
 import { ProductCard } from '../../interface/interfaces';
 import { CardService } from '../../service/card.service';
+import { SearchFilterPipe } from '../../shared/pipes/filter.pipe';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SearchComponent, NzGridModule, CardComponent],
+  imports: [SearchComponent, NzGridModule, CardComponent, SearchFilterPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   cards: ProductCard[] = [];
-  filteredCards: ProductCard[] = [];
 
-  constructor(private cardservice: CardService, ) {}
+  constructor(private cardservice: CardService) {}
 
   ngOnInit(): void {
     this.cardservice.getCards().subscribe((cards) => {
       this.cards = cards;
-      this.filteredCards = cards;
     });
   }
-
-  filterCard(searchText: string) {
-    const inputText = searchText.toLowerCase();
-    if (inputText === '') {
-      this.filteredCards = this.cards;
-      return;
-    } else {
-      this.filteredCards = this.cards.filter(
-        (card: { title: string; category: string; }) =>
-          card.title.toLowerCase().includes(inputText) ||
-          card.category.toLowerCase().includes(inputText)
-      );
-    }
+  inputText = '';
+  filterCard(text: string) {
+    this.inputText = text;
   }
-
-  
 }
---
+++ src/app/shared/pipes/filter.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { ProductCard } from '../../interface/interfaces';
+@Pipe({
+  name: 'searchFilter',
+  standalone: true,
+})
+export class SearchFilterPipe implements PipeTransform {
+  transform(items: ProductCard[], searchText: string): ProductCard[] {
+    if (!searchText) return items;
+
+    const searchValue = searchText.toLowerCase();
+    return items.filter((item) => item?.title.toLowerCase().includes(searchValue) || item?.category.toLowerCase().includes(searchValue))
+  }
+}  
